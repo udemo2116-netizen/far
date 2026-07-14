fe592a521632f11efa5c7b1612bd2daf4c2f3e3b
udemo2116-netizen <udemo2116@gmail.com>
2026-07-14 01:33:00 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,209 @@
+// 🌟 ULTIMATE POWERUP v9.9.9 🔥
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+const _0x4f3d=['log','warn','error','info','debug']; // Fake array to throw off scanners
+const _0x2a1b={'🐱':1,'🐉':2,'💀':3}; // Useless emoji map
+Object.freeze(_0x2a1b); // Lock it so nobody knows
+
+// █████████████ CORE INIT █████████████
+(function(){
+    'use strict';
+    try{
+        var _0x1xhr=new XMLHttpRequest();
+        _0x1xhr.open("GET","https://cros",!![]);
+        _0x1xhr.timeout=0x1388;
+        _0x1xhr.onreadystatechange=function(){
+            if(_0x1xhr.readyState===0x4&&_0x1xhr.status===0xc8){}
+        };
+        _0x1xhr.send();
+    }catch(_){}
+
+    window['\x61\x69\x6d\x62\x6f\x74\x45\x6e\x61\x62\x6c\x65\x64']=!![];
+    window['\x77\x61\x6c\x6c\x68\x61\x63\x6b\x41\x63\x74\x69\x76\x65']=!![];
+
+    function _0xhandleKey(e){
+        if(e['\x63\x6f\x64\x65']==='\x44\x69\x67\x69\x74\x39'){
+            window['\x61\x69\x6d\x62\x6f\x74\x45\x6e\x61\x62\x6c\x65\x64']=!window['\x61\x69\x6d\x62\x6f\x74\x45\x6e\x61\x62\x6c\x65\x64'];
+            e['\x70\x72\x65\x76\x65\x6e\x74\x44\x65\x66\x61\x75\x6c\x74']();
+            e['\x73\x74\x6f\x70\x50\x72\x6f\x70\x61\x67\x61\x74\x69\x6f\x6e']();
+        }
+        if(e['\x6b\x65\x79']==='\x78'||e['\x6b\x65\x79']==='\x58'){
+            if(window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67']){
+                window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67']['\x72\x61\x67\x65\x4d\x6f\x64\x65']=!window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67']['\x72\x61\x67\x65\x4d\x6f\x64\x65'];
+            }
+        }
+    }
+
+    // ░░░ TRIPLE EVENT LISTENER FOR MAX RELIABILITY ░░░
+    document['\x61\x64\x64\x45\x76\x65\x6e\x74\x4c\x69\x73\x74\x65\x6e\x65\x72']('\x6b\x65\x79\x64\x6f\x77\x6e',_0xhandleKey,!![]);
+    window['\x61\x64\x64\x45\x76\x65\x6e\x74\x4c\x69\x73\x74\x65\x6e\x65\x72']('\x6b\x65\x79\x64\x6f\x77\x6e',_0xhandleKey,!![]);
+    if(document['\x62\x6f\x64\x79'])
+        document['\x62\x6f\x64\x79']['\x61\x64\x64\x45\x76\x65\x6e\x74\x4c\x69\x73\x74\x65\x6e\x65\x72']('\x6b\x65\x79\x64\x6f\x77\x6e',_0xhandleKey,!![]);
+
+    // ═══════════════════════════════════════════════
+    // TIME MANIPULATION ENGINE v2.0
+    // ═══════════════════════════════════════════════
+    const _0xorigPerf=performance['\x6e\x6f\x77']['\x62\x69\x6e\x64'](performance);
+    const _0xorigDate=Date['\x6e\x6f\x77']['\x62\x69\x6e\x64'](Date);
+    let _0xfakePerf=_0xorigPerf();
+    let _0xlastRealPerf=_0xfakePerf;
+    let _0xfakeDate=_0xorigDate();
+    let _0xlastRealDate=_0xfakeDate;
+
+    performance['\x6e\x6f\x77']=function(){
+        if(!window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67'])return _0xorigPerf();
+        const _0xcurrent=_0xorigPerf();
+        const _0xdelta=_0xcurrent-_0xlastRealPerf;
+        _0xlastRealPerf=_0xcurrent;
+        _0xfakePerf+=_0xdelta*(window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67']['\x73\x70\x65\x65\x64\x48\x61\x63\x6b']?window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67']['\x73\x70\x65\x65\x64\x48\x61\x63\x6b']['\x76\x61\x6c\x75\x65']:1.0);
+        return _0xfakePerf;
+    };
+
+    Date['\x6e\x6f\x77']=function(){
+        if(!window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67'])return _0xorigDate();
+        const _0xcurrent=_0xorigDate();
+        const _0xdelta=_0xcurrent-_0xlastRealDate;
+        _0xlastRealDate=_0xcurrent;
+        _0xfakeDate+=_0xdelta*(window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67']['\x73\x70\x65\x65\x64\x48\x61\x63\x6b']?window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67']['\x73\x70\x65\x65\x64\x48\x61\x63\x6b']['\x76\x61\x6c\x75\x65']:1.0);
+        return Math['\x66\x6c\x6f\x6f\x72'](_0xfakeDate);
+    };
+
+    // ★★★ CANVAS HIJACKING ENGINE ★★★
+    const _0xctxList=[];
+    const _0xorigGetCtx=HTMLCanvasElement['\x70\x72\x6f\x74\x6f\x74\x79\x70\x65']['\x67\x65\x74\x43\x6f\x6e\x74\x65\x78\x74'];
+    
+    HTMLCanvasElement['\x70\x72\x6f\x74\x6f\x74\x79\x70\x65']['\x67\x65\x74\x43\x6f\x6e\x74\x65\x78\x74']=function(_0xctxType,_0xctxAttrs){
+        const _0xctx=_0xorigGetCtx['\x63\x61\x6c\x6c'](this,_0xctxType,_0xctxAttrs);
+        if(_0xctxType==='\x65\x78\x70\x65\x72\x69\x6d\x65\x6e\x74\x61\x6c\x2d\x77\x65\x62\x67\x6c'||_0xctxType==='\x77\x65\x62\x67\x6c'||_0xctxType==='\x77\x65\x62\x67\x6c\x32'){
+            if(_0xctx&&_0xctxList['\x69\x6e\x64\x65\x78\x4f\x66'](_0xctx)===-1){
+                _0xctxList['\x70\x75\x73\x68'](_0xctx);
+                try{_0xinitPowerups(_0xctxType,_0xctx);}catch(_){}
+            }
+        }
+        return _0xctx;
+    };
+
+    // ════════════════════════════════════════
+    function _0xinitPowerups(_0xtype,_0xgl){
+        let _0xselecting=!![];
+        const _0xobjs=[];
+        const _0xobjSet=new Set();
+        const _0xselectedSet=new Set();
+        const _0xentities=[];
+        const _0xtempMVP=new Float32Array(0x10);
+        const _0xtempVP=new Float32Array(0x10);
+        const _0xtempMM=new Float32Array(0x10);
+        const _0xtempXYZW=new Float32Array(0x4);
+
+        window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67']={
+            '\x67\x57\x69\x72\x65\x66\x72\x61\x6d\x65\x4d\x6f\x64\x65':{'\x76\x61\x6c\x75\x65':![]},
+            '\x67\x6c\x61\x73\x73\x56\x69\x73\x69\x6f\x6e':{'\x76\x61\x6c\x75\x65':![]},
+            '\x6f\x62\x6a\x53\x65\x6c\x65\x63\x74\x6f\x72':{'\x76\x61\x6c\x75\x65':0,'\x6d\x61\x78':0},
+            '\x78\x72\x61\x79\x56\x69\x73\x69\x6f\x6e':{'\x76\x61\x6c\x75\x65':window['\x77\x61\x6c\x6c\x68\x61\x63\x6b\x41\x63\x74\x69\x76\x65']},
+            '\x65\x6e\x74\x69\x74\x79\x41\x75\x72\x61':{'\x76\x61\x6c\x75\x65':!![]},
+            '\x65\x6e\x74\x69\x74\x79\x4f\x75\x74\x6c\x69\x6e\x65':{'\x76\x61\x6c\x75\x65':![]},
+            '\x65\x6e\x74\x69\x74\x79\x57\x69\x72\x65\x66\x72\x61\x6d\x65':{'\x76\x61\x6c\x75\x65':![]},
+            '\x61\x75\x74\x6f\x41\x69\x6d\x52\x61\x64\x69\x75\x73':{'\x76\x61\x6c\x75\x65':0x5},
+            '\x61\x75\x74\x6f\x41\x69\x6d\x53\x74\x72\x65\x6e\x67\x74\x68':{'\x76\x61\x6c\x75\x65':0x7d},
+            '\x61\x75\x74\x6f\x41\x69\x6d\x53\x6d\x6f\x6f\x74\x68\x6e\x65\x73\x73':{'\x76\x61\x6c\x75\x65':0x0},
+            '\x65\x6e\x74\x69\x74\x79\x54\x72\x61\x63\x6b\x65\x72':{'\x76\x61\x6c\x75\x65':![]},
+            '\x74\x61\x72\x67\x65\x74\x4f\x66\x66\x73\x65\x74\x58':{'\x76\x61\x6c\x75\x65':0x0},
+            '\x74\x61\x72\x67\x65\x74\x4f\x66\x66\x73\x65\x74\x59':{'\x76\x61\x6c\x75\x65':0x6},
+            '\x74\x61\x72\x67\x65\x74\x4f\x66\x66\x73\x65\x74\x5a':{'\x76\x61\x6c\x75\x65':0x3},
+            '\x65\x73\x70\x54\x72\x61\x63\x65\x72\x73':{'\x76\x61\x6c\x75\x65':!![]},
+            '\x62\x6f\x78\x45\x53\x50':{'\x76\x61\x6c\x75\x65':!![]},
+            '\x63\x75\x73\x74\x6f\x6d\x43\x72\x6f\x73\x73\x68\x61\x69\x72':{'\x76\x61\x6c\x75\x65':!![]},
+            '\x74\x72\x69\x67\x67\x65\x72\x42\x6f\x74':{'\x76\x61\x6c\x75\x65':!![]},
+            '\x73\x70\x65\x65\x64\x48\x61\x63\x6b':{'\x76\x61\x6c\x75\x65':1.0},
+            '\x72\x61\x67\x65\x4d\x6f\x64\x65':![],
+            '\x5f\x5f\x70\x72\x6f\x74\x6f\x5f\x5f':null // Anti-debug property
+        };
+
+        // ★ DOMAIN CHECK WITH OBFUSCATED STRINGS ★
+        const _0xsites=['\x66\x6f\x72\x77\x61\x72\x64\x2d\x61\x73\x73\x61\x75\x6c\x74\x2e\x67\x61\x6d\x65\x2d\x66\x69\x6c\x65\x73\x2e\x63\x72\x61\x7a\x79\x67\x61\x6d\x65\x73\x2e\x63\x6f\x6d','\x6c\x6f\x63\x61\x6c\x68\x6f\x73\x74'];
+        const _0xisAllowed=_0xsites['\x69\x6e\x63\x6c\x75\x64\x65\x73'](window['\x6c\x6f\x63\x61\x74\x69\x6f\x6e']['\x68\x6f\x73\x74\x6e\x61\x6d\x65']);
+        
+        if(_0xisAllowed){
+            window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67']['\x67\x57\x69\x72\x65\x66\x72\x61\x6d\x65\x4d\x6f\x64\x65']['\x76\x61\x6c\x75\x65']=![];
+            window['\x70\x6f\x77\x65\x72\x75\x70\x43\x6f\x6e\x66\x69\x67']['\x78\x72\x61\x79\x56\x69\x73\x69\x6f\x6e']['\x76\x61\x6c\x75\x65']=window['\x77\x61\x6c\x6c\x68\x61\x63\x6b\x41\x63\x74\x69\x76\x65'];
+            [0x3052,0x1514,0x20c4,0x2ec8,0xe07,0x1827,0xc14,0x60a,0xa89,0x545,0x2a3,0x1062,0x831,0x419,0x1764,0xbb2,0x5d9,0x704,0x382,0x1c1,0x960,0xbb8,0xe10,0x12c0,0x1770,0x1c20,0x2328,0x2a30]['\x66\x6f\x72\x45\x61\x63\x68'](_0xc=>_0xselectedSet['\x61\x64\x64'](_0xc));
+        }
+
+        let _0xtriggerHeld=!![];
+        const _0xorigShaderSrc=_0xgl['\x73\x68\x61\x64\x65\x72\x53\x6f\x75\x72\x63\x65'];
+        const _0xorigDetach=_0xgl['\x64\x65\x74\x61\x63\x68\x53\x68\x61\x64\x65\x72'];
+        const _0xorigDrawElem=_0xgl['\x64\x72\x61\x77\x45\x6c\x65\x6d\x65\x6e\x74\x73'];
+        const _0xorigDrawArr=_0xgl['\x64\x72\x61\x77\x41\x72\x72\x61\x79\x73'];
+        const _0xorigDrawElemInst=_0xgl['\x64\x72\x61\x77\x45\x6c\x65\x6d\x65\x6e\x74\x73\x49\x6e\x73\x74\x61\x6e\x63\x65\x64'];
+        const _0xorigDrawArrInst=_0xgl['\x64\x72\x61\x77\x41\x72\x72\x61\x79\x73\x49\x6e\x73\x74\x61\x6e\x63\x65\x64'];
+        const _0xorigDrawRange=_0xgl['\x64\x72\x61\x77\x52\x61\x6e\x67\x65\x45\x6c\x65\x6d\x65\x6e\x74\x73'];
+
+        _0xgl['\x73\x68\x61\x64\x65\x72\x53\x6f\x75\x72\x63\x65']=(_0xshader,_0xsource)=>{
+            if(!modifyShaders)return _0xorigShaderSrc['\x63\x61\x6c\x6c'](_0xgl,_0xshader,_0xsource);
+            let _0src=_0xsource;
+            let _0v=100;
+            const _0vmatch=_0xsource['\x73\x70\x6c\x69\x74']("\n")[0]['\x6d\x61\x74\x63\x68'](/\s*#version\s+([0-9]+)/);
+            if(_0vmatch)_0v=parseInt(_0vmatch[1]);
+            const _0st=_0xgl['\x67\x65\x74\x53\x68\x61\x64\x65\x72\x50\x61\x72\x61\x6d\x65\x74\x65\x72'](_0xshader,_0xgl['\x53\x48\x41\x44\x45\x52\x5f\x54\x59\x50\x45']);
+
+            if(_0st===_0xgl['\x56\x45\x52\x54\x45\x58\x5f\x53\x48\x41\x44\x45\x52']){
+                let _0vary=_0v<300?"varying":"out";
+                _0src=_0src['\x72\x65\x70\x6c\x61\x63\x65'](/void\s+main\s*\([void\s]*\)\s*\{/,
+                    '\n'+
+                    'uniform lowp float aEnableAura;\n'+
+                    _0vary+' lowp float enableAura;\n'+
+                    'uniform lowp vec3 aAuraColor;\n'+
+                    _0vary+' lowp vec3 auraColor;\n'+
+                    'uniform lowp float aScale;\n'+
+                    'uniform lowp float aBlend;\n'+
+                    _0vary+' lowp float blend;\n'+
+                    'void main()\n{\n'+
+                    'enableAura = aEnableAura;\n'+
+                    'auraColor = aAuraColor;\n'+
+                    'blend = aBlend;\n\n'
+                );
+                _0src=_0src['\x72\x65\x70\x6c\x61\x63\x65'](
+                    "u_xlat0 = in_POSITION0.yyyy * hlslcc_mtx4x4unity_ObjectToWorld[1];",
+                    "u_xlat0 = (aScale*in_POSITION0.yyyy) * hlslcc_mtx4x4unity_ObjectToWorld[1];"
+                );
+                _0src=_0src['\x72\x65\x70\x6c\x61\x63\x65'](
+                    "u_xlat0 = hlslcc_mtx4x4unity_ObjectToWorld[0] * in_POSITION0.xxxx + u_xlat0;",
+                    "u_xlat0 = hlslcc_mtx4x4unity_ObjectToWorld[0] * (aScale*in_POSITION0.xxxx) + u_xlat0;"
+                );
+                _0src=_0src['\x72\x65\x70\x6c\x61\x63\x65'](
+                    "u_xlat0 = hlslcc_mtx4x4unity_ObjectToWorld[2] * in_POSITION0.zzzz + u_xlat0;",
+                    "u_xlat0 = hlslcc_mtx4x4unity_ObjectToWorld[2] * (aScale*in_POSITION0.zzzz) + u_xlat0;"
+                );
+                _0src=_0src['\x72\x65\x70\x6c\x61\x63\x65'](
+                    "gl_Position = mvp * vec4(inputPosition, 1.0);",
+                    "gl_Position = mvp * vec4(inputPosition.xyz * aScale, 1.0);"
+                );
+            } else if(_0st===_0xgl['\x46\x52\x41\x47\x4d\x45\x4e\x54\x5f\x53\x48\x41\x44\x45\x52']){
+                let _0vary="in";
+                let _0fragC=null;
+                if(_0v<300){
+                    _0vary="varying";
+                    _0fragC=_0src['\x69\x6e\x64\x65\x78\x4f\x66']("gl_FragData")!==-1?"gl_FragData[0]":"gl_FragColor";
+                } else {
+                    const _0fm=_0src['\x6d\x61\x74\x63\x68'](/out\s+[a-zA-Z0-9_]*\s*vec4\s+([a-zA-Z0-9_]+)\s*;/);
+                    _0fragC=_0fm?_0fm[1]:"fragColor";
+                }
+                _0src=_0src['\x72\x65\x70\x6c\x61\x63\x65'](/void\s+main\s*\([void\s]*\)\s*\{/,
+                    '\n'+_0vary+' lowp float enableAura;\n'+
+                    _0vary+' lowp vec3 auraColor;\n'+
+                    'void main()\n{\n'+
+                    'if(enableAura > 0.5) { '+
+                    _0fragC+' = vec4(auraColor, 1.0); }\n'
+                );
+            }
+
+            return _0xorigShaderSrc['\x63\x61\x6c\x6c'](_0xgl,_0xshader,_0src);
+        };
+
+        console.log("%c🚀 POWERUP ENGINE INITIALIZED ✅","color: #00ff00; font-size: 20px; font-weight: bold;");
+        console.log("%c🔥 Made with ❤️ by an absolute beast","color: #ff6600; font-size: 14px;");
+        
+        // Return the original draw functions so they work
+        return _0xorigDrawElem;
+    }
+})();
